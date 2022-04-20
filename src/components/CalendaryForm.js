d0770c64e4c3dc4603432f1c96604d043bf6c7c8
Louxz13 <louisomar35@gmail.com>
2022-04-20 14:55:48 -0400

    New

--- src/components/CalendaryForm.js
+++ src/components/CalendaryForm.js
@@ -1,9 +1,21 @@
-import React from 'react';
+import React, {useState} from 'react';
+import Calendar from 'react-calendar';
+import 'react-calendar/dist/Calendar.css';
+//import './index.css';
 
-const CalendarForm = () => {
+function CalendarForm() {
+  const [value, onChange] = useState(new Date());
+
   return (
-    <div>CalendarForm</div>
-  )
+    <div>
+      <h1>Horarios</h1>
+      
+
+
+
+      <Calendar onChange={onChange} value={value} className="calendario"/>
+    </div>
+  );
 }
 
 export default CalendarForm
--- src/components/navbar/NavBar.js
+++ src/components/navbar/NavBar.js
@@ -1,12 +1,16 @@
-import React from 'react';
+import React, {useState} from 'react';
 import { Link } from 'react-router-dom';
 import "./NavBar.css";
 
 const NavBar = () => {
+
+    const  [isMobile, setIsMobile] = useState(false);
   return ( // prueba
     <nav className='navbar'>
         <h3 className='logo'>Logo</h3>
-        <ul className='nav-links'>
+        <ul className={isMobile? "nav-links-mobile" : "nav-links"}
+        onClick={()=> setIsMobile(false)}
+        >
             <Link to="/homeform" className='home'>
                 <li>Home</li>
             </Link>
@@ -20,6 +24,15 @@ const NavBar = () => {
                 <li>About</li>
             </Link>
         </ul>
+        <button className='mobile-menu-icon'
+        onClick={() => setIsMobile(!isMobile)}
+        >
+            {isMobile ? (
+                <i className='fas fa-times'></i> 
+            ) : (
+                <i className='fas fa-bars'></i>
+            )}
+        </button>
     </nav>
   )
 }
