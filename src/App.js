141de64ba5f147c29cb6574ddde39693713f58b3
Louxz13 <louisomar35@gmail.com>
2022-04-20 15:20:15 -0400

    login added

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {BrowserRouter, Routes, Route } from "react-router-dom";
+import LoginForm from './LoginForm';
 import HomeForm from "./components/HomeForm";
 import CalendaryForm from "./components/CalendaryForm";
 import ContactForm from "./components/ContactForm";
@@ -7,8 +8,35 @@ import AboutForm from "./components/AboutForm";
 //import SignUpForm from "./components/SignUpForm";
 import NavBar from './components/navbar/NavBar';
 
-const App = () => {
+function App() {
+
+  const adminUser = {
+    email: "admin@admin.com",
+    password: "admin123"
+  }
+ 
+  const [user,setUser] = useState({name:"",email:""});
+  const [error, setError] = useState("");
+ 
+  const Login = details => {
+    console.log(details);
+ 
+    if (details.email == adminUser.email && details.password == adminUser.password) {
+      console.log("Logged in");
+      setUser({
+        name: details.name,
+        email: details.email
+      });
+     } else {
+       console.log("Details do not match");
+       setError("Details do not match");
+     }
+  }
+
   return (
+    <div className="App">
+   {(user.email != "") ? (
+
     <BrowserRouter>
       <NavBar />
 
@@ -19,6 +47,11 @@ const App = () => {
         <Route path="/AboutForm" element={<AboutForm />} exact/>
       </Routes>
     </BrowserRouter>
+
+    ) : (
+      <LoginForm Login={Login} error={error}/> 
+    )}
+  </div>
   );
 };
 
